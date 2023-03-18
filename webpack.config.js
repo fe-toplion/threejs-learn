915c25a91c8ed127b1ce059c17c941b8494f9a81
zhaowenlong <zhaowenlong@kuaishou.com>
2023-03-18 22:25:04 +0800

    init

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,23 @@
-let path = require("path");
+const path = require("path");
+const fs = require('fs');
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-// 不同页面的名字
-const pageNames = ["threejs", "photoSphereViewer"];
+const pageNames = fs.readdirSync(`${__dirname}/src`);
+
+const getEntry = () => {
+  const entry = {};
+  pageNames.forEach(item => {
+    entry[item] = path.resolve(__dirname, 'src', item, 'index.js');
+  });
+  return entry;
+}
 
 // 创建 new HtmlWebpackPlugin({...}) 的工厂函数
 const HtmlpluginArr = (pageNames) => {
   return pageNames.map(
     (pageName) =>
       new HtmlWebpackPlugin({
-        template: `./src/${pageName}/index.html`,
+        template: `${__dirname}/public/index.html`,
         filename: `${pageName}/index.html`, // 必须加后缀名，不可使用[ext]
         chunks: [`${pageName}`], // 指定入口文件，与entry中的key一致
         hot: true, // 开启热更新
@@ -19,10 +27,7 @@ const HtmlpluginArr = (pageNames) => {
 
 module.exports = {
   mode: "development",
-  entry: {
-    threejs: "./src/threejs/index.js",
-    photoSphereViewer: "./src/photoSphereViewer/index.js",
-  },
+  entry: getEntry(),
   output: {
     filename: "[name]/index.js",
     path: path.resolve("dist"),
